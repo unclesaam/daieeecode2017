4dcd49e1374941a797dc27558518bb1225a52d4d
Sam <s.c_123@hotmail.com>
2017-03-23 16:04:35 -0400

    Cleaned sorter function

--- Challenge 1 and 2/sorter.py
+++ Challenge 1 and 2/sorter.py
@@ -1,10 +1,12 @@
-def find(nom_scramble):
+from pokedex import Pokedex
+import re
+
+def find_pokemon_name(nom_scramble):
  
     pokedex = Pokedex()
     nom_scramble = nom_scramble.lower()
  
     for poke in pokedex.pokemons:
-        #print("%s, %s"%(len(poke.name), len(nom)))
         pokeNom = list(poke.name.lower())
         for lettre1 in nom_scramble:
             if lettre1 in pokeNom:
@@ -15,17 +17,12 @@ def find(nom_scramble):
         if not pokeNom:
             return poke.name
  
- 
- 
- 
     return "no pokemon found"
  
- 
- 
- 
- 
+
  
 if __name__ == "__main__":
-    print('sorter')
- 
-print(find("sqitlure"))
+    with open('pokemon_names_to_reorder.txt', "r") as f:
+        for name in f:
+            name_clean = re.sub("\n","",name)
+            print("%s: %s"%(name_clean,find_pokemon_name(name_clean))) 
