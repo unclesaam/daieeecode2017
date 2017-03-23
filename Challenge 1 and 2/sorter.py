c101096e2788d068f179116159d1a9c4983f30bc
Sam <s.c_123@hotmail.com>
2017-03-23 15:59:07 -0400

    Started challenge 4

--- Challenge 1 and 2/sorter.py
+++ Challenge 1 and 2/sorter.py
@@ -1,5 +1,31 @@
-from pokedex import Pokedex
-
-
+def find(nom_scramble):
+ 
+    pokedex = Pokedex()
+    nom_scramble = nom_scramble.lower()
+ 
+    for poke in pokedex.pokemons:
+        #print("%s, %s"%(len(poke.name), len(nom)))
+        pokeNom = list(poke.name.lower())
+        for lettre1 in nom_scramble:
+            if lettre1 in pokeNom:
+                pokeNom.remove(lettre1)
+            else:
+                break
+ 
+        if not pokeNom:
+            return poke.name
+ 
+ 
+ 
+ 
+    return "no pokemon found"
+ 
+ 
+ 
+ 
+ 
+ 
 if __name__ == "__main__":
-	print('sorter')
+    print('sorter')
+ 
+print(find("sqitlure"))
