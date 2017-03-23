cffd8a40af362a5a8b54199b83c8dfac3a455e36
Sam <s.c_123@hotmail.com>
2017-03-23 17:28:47 -0400

    Added #4

--- Challenge 4/main.cpp
+++ Challenge 4/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <fstream>
-
+#include <string>
+ 
+ 
 using namespace std;
-
-
-
-int main(){
-	
+ 
+ 
+ 
+int main () {
+ 
+    string texte;
+    string file_contents;
+    string mot;
+    ifstream file;
+    file.open ("data");
+ 
+    while(!file.eof())
+    {
+        getline(file,texte);
+        file_contents += texte;
+        file_contents.push_back('\n');
+ 
+    }
+    /*Faire une liste de tous les mots qui apparaissent dans le texte*/
+    /*Itérer sur cette liste, créer une nouvelle colonne pour un nouveau mot*/
+    /*Ajouter au compteur pour un mot déjà existant*/
+ 
+ 
+ 
+ 
+    cout << mot << endl;
+ 
+ 
+ 
+ 
+ 
+    /*cout << file_contents;*/
+ 
+    return 0;
+ 
+ 
 }
